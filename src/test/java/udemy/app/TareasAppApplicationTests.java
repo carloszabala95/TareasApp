296a36a50e8acc1b73c2d5fcf3124fc6e742458e
Carlos Zabala <cazabalab@unal.edu.co>
2025-06-09 23:47:28 -0500

    Se agrega proyecto

---
+++ src/test/java/udemy/app/TareasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
